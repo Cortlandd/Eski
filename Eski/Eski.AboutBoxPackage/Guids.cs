f75ba141037747d68886cccbceeede29260f76bc
Cortland Walker <cortlandits@gmail.com>
2016-12-27 10:14:46 -0600

    Add project files.

---
+++ Eski/Eski.AboutBoxPackage/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace Eski.AboutBoxPackage
+{
+    static class GuidList
+    {
+        public const string guidAboutBoxPackagePkgString = "8048ef35-f084-499a-9cb5-d125b8be3176";
+        public const string guidAboutBoxPackageCmdSetString = "2f979de4-2e0c-4f83-b9d3-c5243eea4907";
+
+        public static readonly Guid guidAboutBoxPackageCmdSet = new Guid(guidAboutBoxPackageCmdSetString);
+    };
+}
